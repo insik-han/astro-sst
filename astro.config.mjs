34fa6e602cac087d7cd85d896f9ffda018f94b74
insik-han <gksdudrms0@icloud.com>
2023-03-12 16:19:02 +0900

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 import aws from "astro-sst/lambda";
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: aws(),
+  integrations: [tailwind()]
 });
